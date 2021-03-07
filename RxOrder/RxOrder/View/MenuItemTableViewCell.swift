db698b8c013629c8dd695c706b1adab70aac0d7d
so-soon <hyuk0610@gmail.com>
2021-03-07 19:38:23 +0900

    add : MenuTableViewCell , fix: info.plist not found

---
+++ RxOrder/RxOrder/View/MenuItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuItemTableViewCell.swift
+//  RxOrder
+//
+//  Created by Randy on 2021/03/07.
+//
+
+import UIKit
+
+class MenuItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
