22bbc302e9380c80103144e6d558e782f31d97f7
so-soon <hyuk0610@gmail.com>
2021-03-08 16:46:08 +0900

    Impl : init project

---
+++ RxFacebookMe/Pods/Target Support Files/Pods-RxFacebookMe-RxFacebookMeUITests/Pods-RxFacebookMe-RxFacebookMeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxFacebookMe_RxFacebookMeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RxFacebookMe_RxFacebookMeUITests
+@end
---
+++ RxFacebookMe/Pods/Target Support Files/Pods-RxFacebookMeTests/Pods-RxFacebookMeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxFacebookMeTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxFacebookMeTests
+@end
